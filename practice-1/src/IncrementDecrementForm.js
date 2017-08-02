0a5afebb30bc1123137f7fc08b6f82911f09e80a
Canh Pham <canh.pham@asnet.com.vn>
2017-08-02 17:34:01 +0700

    Create increment/decrement app and update README file

---
+++ practice-1/src/IncrementDecrementForm.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+function IncrementDecrementButton(props) {
+  return (
+    <div className='button'>
+      <button onClick={props.handleDecrementClick}> &#45; </button>
+      <button onClick={props.handleIncrementClick}> &#43; </button>
+    </div>
+  );
+}
+
+function NumberResult(props) {
+  return (
+    <span className='number-result'>{props.numberResult}</span>
+  );
+}
+
+class IncrementDecrementForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {result: 0};
+  }
+
+  handleIncrementButtonClick = () => this.setState(preState => ({result: preState.result + 1}))
+
+  handleDecrementButtonClick = () => this.setState(preState => ({result: preState.result - 1}))
+
+  render() {
+    return (
+      <fieldset className='wrapper-content'>
+        <legend className='legend-title'>Increment/Decrement Application</legend>
+        <NumberResult
+          numberResult={this.state.result} />
+        <IncrementDecrementButton
+          handleIncrementClick={this.handleIncrementButtonClick}
+          handleDecrementClick={this.handleDecrementButtonClick} />
+      </fieldset>
+    );
+  }
+}
+
+export default IncrementDecrementForm;

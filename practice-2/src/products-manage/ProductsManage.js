7623408cb833a6dd41e2ee35618c7f44c0c25fde
Canh Pham <canh.pham@asnet.com.vn>
2017-08-18 14:25:47 +0700

    Update README file and update some UI

--- practice-2/src/products-manage/ProductsManage.js
+++ practice-2/src/products-manage/ProductsManage.js
@@ -201,7 +201,7 @@ class ProductsManage extends React.Component {
   }
 
 	render() {
-    console.log('this.state', this.state)
+    console.log('this.state', this.state);
 		return (
       <div className='products-manage'>
         <h1>

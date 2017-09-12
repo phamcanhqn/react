65300dbd40d7e7cfb966dfffc5616fbdba997e8a
Canh Pham <canh.pham@asnet.com.vn>
2017-09-12 16:11:17 +0700

    Create structure and folders

---
+++ practice-3/src/components/commons/button/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style/Button.css'
+
+const Button = props => {
+  return (
+    <button
+      type="button"
+      name={props.name}
+      className={props.className}
+      onClick={props.handleClick}>
+      {props.label}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string,
+  className: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+Button.defaultProps = {
+  name: '',
+  className: ''
+};
+
+export { Button };
